ababd0fd8507884c7ab3486647fdadd4b63f62cb
David Alexander <david@ternsoftware.org>
2018-07-10 22:08:25 +1000

    added rename script v1

---
+++ rename-vms.ps1
@@ -0,0 +1,16 @@
+
+$vms = get-azurermvm -resourcegroupname DAVIDALEXANDER | where Name -Like "da-rhel-03"
+foreach ($vm in $vms) {
+  write-output $VM.Name 
+  $location = $vm.Location
+  $vmName = read-host -prompt "Enter the new server name for this server"
+  $osDisk = get-azurermdisk | where { $_.ManagedBy -eq $vm.Id }
+  $osDisk
+  $nic = get-azurermnetworkinterface | where { $_.VirtualMachine.Id -eq $vm.Id }
+  $nic
+  $vmConfig = new-azurermvmconfig -VMName $vmName -VMSize "Standard_B2s"
+  $newVM = Add-AzureRmVMNetworkInterface -VM $vmConfig -Id $nic.Id
+  $newVM = Set-AzureRmVMBootDiagnostics -disable -VM $newVM
+  remove-azurermvm -resourcegroupname DAVIDALEXANDER -name $vm.Name
+  New-AzureRmVM -resourcegroupname DAVIDALEXANDER -Location $location -VM $newVM
+}
